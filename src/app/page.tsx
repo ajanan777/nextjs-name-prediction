fd317b2bd5fb60812aa22c2e0229af54423a2bf8
Ajanan <ajanankumariah7@gmail.com>
2025-08-03 17:34:59 +0100

    Added use client, usestate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+"use client";
+import { useState } from "react"; 
+
 export default function Home() {
   return (<div> 
     <div>
       <h1> Enter your name:</h1>
     </div>
+    <form>
+      <input type="text" placeholder="Type your name..." />
+      <button type="submit"> Predict Data</button>
+    </form>
     </div>
   );
 }
